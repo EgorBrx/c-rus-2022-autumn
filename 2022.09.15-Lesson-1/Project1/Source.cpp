29696e23fbc27a13df0d62a95461d6946a5058a6
st021213 <st021213@student.spbu.ru>
2022-09-15 12:01:10 +0300

    Lesson 1(2) initial commit

---
+++ 2022.09.15-Lesson-1/Project1/Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+//using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+	std::cout << "HELLO WORLD" << std::endl;
+
+	return EXIT_SUCCESS;
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # c-rus-2022-autumn
+Programming in C
